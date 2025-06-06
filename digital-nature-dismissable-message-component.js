c9e0bfe0232b336a30951e695994dbd0cbb326bc
Gareth Midwood <gareth@digital-nature.co.uk>
2025-06-06 16:04:55 +0100

    Initial import

---
+++ digital-nature-dismissable-message-component.js
@@ -0,0 +1,21 @@
+import DigitalNatureWebComponent from "@digital-nature-ltd/web-component";
+import template from './digital-nature-dismissable-message-component-template.html?raw';
+
+export default class DigitalNatureDismissableMessageComponent extends DigitalNatureWebComponent
+{
+    static tagName = 'digital-nature-dismissable-message';
+
+    constructor() {
+        super({template: template});
+
+        this.addEventListener("click", this);
+    }
+
+    handleClick (event)
+    {
+        this.remove();
+    }
+}
+
+// add the custom element to the registry
+customElements.define(DigitalNatureDismissableMessageComponent.tagName, DigitalNatureDismissableMessageComponent);
