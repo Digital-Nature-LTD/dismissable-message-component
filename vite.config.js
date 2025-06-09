a26e872100135383372371a88067d11649fada4a
Gareth Midwood <gareth@digital-nature.co.uk>
2025-06-09 21:17:51 +0100

    Restructure to use vite

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import path from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    base: "./",
+    plugins: [],
+    build: {
+        sourcemap: true,
+        lib: {
+            entry: path.resolve(__dirname, "src/index.js"),
+            name: "mylib",
+            formats: ["es", "cjs", "umd", "iife"],
+            fileName: (format) => `index.${format}.js`,
+        },
+        // rollupOptions: {
+        //     external: [],
+        //     output: {
+        //         globals: {
+        //
+        //         },
+        //     },
+        // },
+    },
+});
