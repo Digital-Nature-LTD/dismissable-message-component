42193e2540df4ea1f69b848bb4324dbbc68782ee
Gareth Midwood <gareth@digital-nature.co.uk>
2025-06-10 14:12:19 +0100

    Add test page. Add live reload when testing. Fix logo import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import DigitalNatureWebComponent from "@digital-nature-ltd/web-component";
 import template from './digital-nature-dismissable-message-component-template.html?raw';
 
+console.log("DigitalNatureDismissableMessageComponent");
+
 export default class DigitalNatureDismissableMessageComponent extends DigitalNatureWebComponent
 {
     static tagName = 'digital-nature-dismissable-message';
@@ -16,6 +18,8 @@ export default class DigitalNatureDismissableMessageComponent extends DigitalNat
         this.remove();
     }
 }
+
+console.log("DigitalNatureDismissableMessageComponent adding to registry");
 
 // add the custom element to the registry
 customElements.define(DigitalNatureDismissableMessageComponent.tagName, DigitalNatureDismissableMessageComponent);
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,26 @@
 import path from "path";
 import { defineConfig } from "vite";
+import livePreview from 'vite-live-preview'
 
 export default defineConfig({
-    base: "./",
-    plugins: [],
+    base: "",
+    plugins: [
+        livePreview({}),
+    ],
+    preview: {},
     build: {
+        assetsInlineLimit: Infinity, // inline css images
         sourcemap: true,
         lib: {
             entry: path.resolve(__dirname, "src/index.js"),
-            name: "mylib",
+            name: "dn_dismissable_message_component",
             formats: ["es", "cjs", "umd", "iife"],
             fileName: (format) => `index.${format}.js`,
+        },
+        rollupOptions: {
+            input: {
+                'index.html': 'index.html',
+            },
         },
         // rollupOptions: {
         //     external: [],
